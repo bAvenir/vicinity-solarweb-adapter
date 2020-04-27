12f131fc9aa8307a8e6a364d216025322b5af6b3
Jorge Almela <jorgealmela@JorgePc.local>
2020-04-27 17:03:28 +0200

    All requests go now through NGINX

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const express = require('express'),
 // Configure express to work with proxy and rate-limit
 // Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
 // see https://expressjs.com/en/guide/behind-proxies.html
-// app.set('trust proxy', 1);
+server.set('trust proxy', 1);
 
 // Load loggers
 const morgan = require('morgan'),
